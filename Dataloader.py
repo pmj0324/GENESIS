7f483b1505f018dc433064d2123e52f5aaaa3322
Minje Park <pmj0324@g.skku.edu>
2025-06-26 15:33:02 +0900

    Update Dataloader.py

--- Dataloader.py
+++ Dataloader.py
@@ -1,3 +1,7 @@
+import torch
+import numpy
+import h5py
+
 class PMTDiffusionDataset(Dataset):
     def __init__(
         self,
@@ -6,16 +10,14 @@ class PMTDiffusionDataset(Dataset):
         div1=50000.0,
         shift0=0.0,
         shift1=0.0,
-        mul0=1.0,
-        mul1=1.0,
         scale_first_time="none"  # 'none', 'log'
     ):
         with h5py.File(h5_path, 'r') as f:
             x = f['x'][:]         # (N, 2, 5160)
             label = f['label'][:] # (N, 3)
-        # 채널별 normalization, shift, 곱셈
-        x[:, 0, :] = ((x[:, 0, :] - shift0) / div0) * mul0
-        x[:, 1, :] = ((x[:, 1, :] - shift1) / div1) * mul1
+        # 채널별 normalization: (x / div) - shift
+        x[:, 0, :] = (x[:, 0, :] / div0) - shift0
+        x[:, 1, :] = (x[:, 1, :] / div1) - shift1
 
         # firstTime 스케일 조정 (x[:,1,:])
         if scale_first_time == "log":
@@ -30,5 +32,6 @@ class PMTDiffusionDataset(Dataset):
 
     def __len__(self):
         return len(self.x)
+
     def __getitem__(self, idx):
         return torch.from_numpy(self.x[idx]).float(), torch.from_numpy(self.label[idx]).float()
